682f495013de51e35b8ef2dd3ec2396b97596a35
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-23 14:58:13 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    activity = `roller-skate`;
+  }
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(Work) {
+  if (Work === undefined) {
+    Work = `go to the office`;
+  }
+  return `This Monday, I will ${Work}.`;
+}
+
+function wrapAdjective(Wrap) {
+  if (Wrap === undefined) {
+    Wrap = "*";
+  }
+  const innerFunction = function (nicePhrase) {
+    if (nicePhrase === undefined) {
+     nicePhrase = "special";
+    }
+    return `You are ${Wrap}${nicePhrase}${Wrap}!`;
+  };
+  return innerFunction;
+}
